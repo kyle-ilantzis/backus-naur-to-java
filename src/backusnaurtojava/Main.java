17ad0298561c5aa56e87cf064e1f34fae1e9333e
kyle-ilantzis <ilantzis.kyle@gmail.com>
2015-06-04 17:12:51 -0400

    in example main, report parse errors to stderr

--- src/backusnaurtojava/Main.java
+++ src/backusnaurtojava/Main.java
@@ -9,12 +9,21 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, CFGParser.SyntaxError {
+    public static void main(String[] args) throws IOException {
 
         InputStream in = Main.class.getResourceAsStream("expr.cfg");
 
         CFGParser cfgParser = new CFGParser(new InputStreamReader(in));
-        cfgParser.parseGrammar();
+
+        try {
+            cfgParser.parseGrammar();
+        } catch (CFGParser.SyntaxError e) {
+            for (String error : e.getErrors()) {
+                System.err.println(error);
+            }
+            System.exit(1);
+        }
+
         Grammar grammar = cfgParser.grammar();
 
         System.out.println(grammar.productions());
